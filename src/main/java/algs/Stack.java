08f2eaf3b06302d9fc10bb6f25e2e2a8e83a74c7
ZY <zhyuxush@126.com>
2014-08-19 23:21:47 +0800

    stack

--- src/main/java/algs/Stack.java
+++ src/main/java/algs/Stack.java
@@ -6,6 +6,49 @@
 
 package algs;
 
-public class Stack
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item>
 {
+    private static final int INIT_CAPACITY = 2;
+    private static final int RESIZE_FACTOR = 2;
+    private Object[] items;
+    private int top;
+
+    public Stack()
+    {
+        items = new Object[INIT_CAPACITY];
+    }
+
+    public void push(Item item)
+    {
+        if (top == items.length - 1)
+        {
+            resize();
+        }
+        items[top++] = item;
+    }
+
+    private void resize()
+    {
+        Object[] newItems = new Object[items.length * RESIZE_FACTOR];
+        System.arraycopy(items, 0, newItems, 0, items.length);
+        items = newItems;
+    }
+
+    public Item pop()
+    {
+        return (Item)items[--top];
+    }
+
+    public int size()
+    {
+        return top;
+    }
+
+    @Override
+    public Iterator<Item> iterator()
+    {
+        return null;
+    }
 }
--- src/main/java/algs/test/TestStack.java
+++ src/main/java/algs/test/TestStack.java
@@ -6,6 +6,21 @@
 
 package algs.test;
 
+import algs.Stack;
+
 public class TestStack
 {
+    public static void main(String[] args)
+    {
+        Stack<String> stack = new Stack<>();
+        stack.push("h");
+        stack.push("e");
+        stack.push("l");
+        stack.push("l");
+
+        for (int i = 0; i < 4; i++)
+        {
+            System.out.println(stack.pop());
+        }
+    }
 }
